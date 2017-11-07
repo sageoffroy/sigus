d4f3d47e701663bb27474db771c78c801e578b6b
Silvio Geoffroy <sageoffroy@gmail.com>
2017-11-07 11:45:06 -0300

    report_fields has_one observations

--- app/models/report_detail.rb
+++ app/models/report_detail.rb
@@ -1,12 +1,12 @@
 class ReportDetail < ApplicationRecord
   belongs_to :report
   belongs_to :agent
-  has_many :observations
-  has_many :rotative_turns
+  has_one :observation
+  has_one :rotative_turn
   validates :agent, uniqueness: true
 
-  accepts_nested_attributes_for :rotative_turns
-  accepts_nested_attributes_for :observations
+  accepts_nested_attributes_for :rotative_turn
+  accepts_nested_attributes_for :observation
   
   
 
