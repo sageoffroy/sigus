bde3c08dbaef3f11b4b0e2c2aafc9a59dd3e447b
Silvio Geoffroy <sageoffroy@gmail.com>
2018-03-28 12:55:12 -0300

    carga de oficinas de cobro

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,40 +31,7 @@ user = User.new
 user.email = 'direccionhospitalrawson@gmail.com'
 user.password = 'direccion_hospital'
 user.dependence = Dependence.where(code:106).first
-user.save!
-user.add_role 'director'
-
-user = User.new
-user.email = 'direccionhospitalalvear@gmail.com'
-user.password = 'direccion_hospital'
-user.dependence = Dependence.where(code:456).first
-user.save!
-user.add_role 'director'
-
-user = User.new
-user.email = 'direccionapnorte@gmail.com'
-user.password = 'areanorte'
-user.dependence = Dependence.where(code:104).first
-user.save!
-user.add_role 'directora'
-
-user = User.new
-user.email = 'direccionareasur@gmail.com'
-user.password = 'areapsur'
-user.dependence = Dependence.where(code:143).first
-user.save!
-user.add_role 'directora'
-
-user = User.new
-user.email = 'direccionareatrelew@gmail.com'
-user.password = 'areatrelew'
-user.dependence = Dependence.where(code:103).first
-user.save!
-user.add_role 'directora'
-
-user = User.new
-user.email = 'direccionareaesquel@gmail.com'
-user.password = 'areaesquel'
-user.dependence = Dependence.where(code:121).first
-user.save!
-user.add_role 'directora'
+Agent.all.each do |a|
+	a.oficina = a.agent_of_services.first.service_of_dependence.dependence.code
+	a.save
+end
